042fa8ed96191304aeab6d75cb40a31b8b4c5382
Lucas Sales <Lucasricardo525@gmail.com>
2025-07-15 14:58:27 -0300

    primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"], // ajuste conforme seus arquivos
+  theme: {
+    extend: {
+      fontFamily: {
+        kalam: ["Kalam", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
